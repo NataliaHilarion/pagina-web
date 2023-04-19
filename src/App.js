9abaf0d69d4e58cdfacb71a2ddbe02b3374e094e
Natalia Hilarion <natalia.hilarion@outlook.com>
2023-04-19 13:01:06 -0500

    creando los link del menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Citas from "./components/citas";
+import Episodios from "./components/episodios";
+import Home from "./components/home";
+import Menu from "./components/menu";
+import Personajes from "./components/personajes";
 
 function App() {
+  const currentURL = window.location.pathname;
+  function renderContent() {
+    switch (currentURL) {
+      case "/":
+        return <Home/>
+      case "/personajes":
+        return <Personajes />
+      case "/citas":
+        return <Citas/>;
+      case "/episodios":
+        return <Episodios/>
+      default:
+       return <Home />;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      {renderContent()}
     </div>
   );
 }
---
+++ src/components/menu-items.js
@@ -0,0 +1,9 @@
+function MenuItem(props){
+  return (
+    <li>
+      <a href={props.link}>{props.text}</a>
+    </li>
+  );
+}
+
+export default MenuItem
